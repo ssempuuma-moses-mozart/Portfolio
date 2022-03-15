d452a1761083586b33978ea8a5a645691144fd47
ssempuuma-moses-mozart <57084715+ssempuuma-moses-mozart@users.noreply.github.com>
2022-03-15 09:36:41 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'brand':"#28A745",
+        'accent':"#76FA23"
+      },
+      backgroundImage: {
+        'background': "url('/src/img/hero-bg.jpg')"
+      },
+    },
+  },
+  plugins: [],
+}
